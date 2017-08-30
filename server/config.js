143d9bc4276a455dcac5b492d5a2733c47648d06
Luizleb <luizleb@gmail.com>
2017-08-30 18:30:04 -0300

    Header Styles
    Header background image and general layout

--- server/config.js
+++ server/config.js
@@ -1,9 +1,15 @@
+var express = require('express');
 var exphbs = require('express-handlebars');
 var routes = require('./routes');
+var path = require('path');
 
 module.exports = function(app){
+
 // define the router
     app.use(routes);
+
+// define the public path
+    app.use('/assets', express.static(path.join(__dirname , '../public')));
 
 // define handlebars as the view engine
     app.engine('handlebars',exphbs.create({
