cfc1ca20566c8f3994541d373c7578245be4e787
LEB <luizleb@gmail.com>
2017-08-31 14:48:25 -0300

    File Upload Improved

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,7 @@
 // require : main
 var express = require('express');
 var router = express.Router();
-var path = require('path');
-var multer = require('multer');
-var uploadMulter = multer({
-    dest: path.join(__dirname, '../public/uploads'),
-  });
+var uploadFile = require('../controllers/fileUpload');
 // require : controllers
 var home = require('../controllers/home');
 var upload = require('../controllers/upload');
@@ -13,6 +9,6 @@ var upload = require('../controllers/upload');
 // routing
 router.get('/', home.index);
 router.get('/uploadGet', upload.indexGet);
-router.post('/uploadPost', uploadMulter.single('file'), upload.indexPost);
+router.post('/uploadPost', uploadFile.single('file'), upload.indexPost);
 
 module.exports = router;
