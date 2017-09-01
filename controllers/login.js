050ced164fb2de0b1cdc6eab8d146a2de43c05f9
LEB <luizleb@gmail.com>
2017-09-01 19:30:40 -0300

    Login and Register
    Login and Register page (html, css and js)- no database connection yet.

---
+++ controllers/login.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    indexGet: function(req, res) {
+        res.render('login');
+    }
+};
--- server/routes.js
+++ server/routes.js
@@ -5,9 +5,11 @@ var uploadFile = require('../controllers/fileUpload');
 // require : controllers
 var home = require('../controllers/home');
 var upload = require('../controllers/upload');
+var login = require('../controllers/login');
 
 // routing
 router.get('/', home.index);
+router.get('/login', login.indexGet);
 router.get('/uploadGet', upload.indexGet);
 router.post('/uploadPost', uploadFile.single('file'), upload.indexPost);
 
